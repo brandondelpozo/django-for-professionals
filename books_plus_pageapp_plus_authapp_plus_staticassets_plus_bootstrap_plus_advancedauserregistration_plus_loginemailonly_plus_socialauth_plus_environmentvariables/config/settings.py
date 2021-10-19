250a614deff697bff7765dc2a1fedcc8564df1e2
brandondelpozo <branzzel@gmail.com>
2021-10-19 02:11:43 -0500

    create environment variables

--- books_plus_pageapp_plus_authapp_plus_staticassets_plus_bootstrap_plus_advancedauserregistration_plus_loginemailonly_plus_socialauth_plus_environmentvariables/config/settings.py
+++ books_plus_pageapp_plus_authapp_plus_staticassets_plus_bootstrap_plus_advancedauserregistration_plus_loginemailonly_plus_socialauth_plus_environmentvariables/config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env # new
+
+env = Env() # new
+env.read_env() # new
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&3yd&yszxsq6ar2tsv#5k1+oheevma$l1mi+3$$dl8gos%-+q3'
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DJANGO_DEBUG") # new
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['herokuapp.com', 'localhost', '127.0.0.1'] # new
 
 
 # Application definition
@@ -87,16 +91,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
-    }
+    "default": env.dj_db_url("DATABASE_URL", 
+    default="postgres://postgres@db/postgres")
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
