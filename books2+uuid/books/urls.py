c77260c4e0b35cb0fd8321df93e01568244e4777
brandondelpozo <branzzel@gmail.com>
2021-10-29 23:58:31 -0500

    update urls.py

--- books2+uuid/books/urls.py
+++ books2+uuid/books/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BookListView, BookDetailView # new
+from .views import BookListView, BookDetailView
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list'),
