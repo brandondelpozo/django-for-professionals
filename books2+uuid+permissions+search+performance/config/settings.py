07bf422a6ce2049fa8a8c538d7f11f27f7bf4bbd
brandondelpozo <branzzel@gmail.com>
2022-01-25 23:51:42 -0500

    update settigs.py

--- books2+uuid+permissions+search+performance/config/settings.py
+++ books2+uuid+permissions+search+performance/config/settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware', # new
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 CACHE_MIDDLEWARE_ALIAS = 'default'
